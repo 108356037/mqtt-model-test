f6e243c27fd38b1b054f6e56878b953c916fee6b
蔡維哲 <tsai_wei_che@caiweizhede-MacBook-Pro.local>
2020-10-29 17:22:26 +0800

    first commit

---
+++ py_scripts/publish.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import random
+import json
+import datetime
+import time
+
+ISOTIMEFORMAT = '%m/%d %H:%M:%S'
+
+client = mqtt.Client()
+
+client.username_pw_set("try","xxx")
+
+client.connect("localhost", port=1883, keepalive=60)
+
+while True:
+    t0 = random.randint(0,30)
+    t = datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    payload = {'Temperature' : t0, 'Time' : t}
+    print(json.dumps(payload))
+    client.publish("Try/MQTT", json.dumps(payload))
+    time.sleep(2.5)
---
+++ py_scripts/subscribe.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("Try/MQTT")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + msg.payload.decode("utf-8"))
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+
+client.on_message = on_message
+
+client.username_pw_set("try", "xxx")
+
+client.connect("localhost", port=1883, keepalive=60)
+
+client.loop_forever()
+
+    
