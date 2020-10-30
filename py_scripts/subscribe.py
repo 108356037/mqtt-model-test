21b2c8a9fe878c165e0f246144ec0138c13b6a0f
108356037 <108356037@nccu.edu.tw>
2020-10-30 20:40:53 +0800

    some minor changes

--- py_scripts/subscribe.py
+++ py_scripts/subscribe.py
@@ -1,12 +1,15 @@
 import paho.mqtt.client as mqtt
 
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    client.subscribe("Try/MQTT")
+    client.subscribe("mota/getContainerStatus")
+
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + msg.payload.decode("utf-8"))
+
 
 client = mqtt.Client()
 
@@ -19,5 +22,3 @@ client.username_pw_set("try", "xxx")
 client.connect("localhost", port=1883, keepalive=60)
 
 client.loop_forever()
-
-    
