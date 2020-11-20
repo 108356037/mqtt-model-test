0ab90cb444d596263b76c8626d7fec4e8d1330fb
108356037 <108356037@nccu.edu.tw>
2020-11-20 21:05:29 +0800

    need to complete the cloud publish module

--- python_mods/cloud/cloud_publisher.py
+++ python_mods/cloud/cloud_publisher.py
@@ -1,18 +1,29 @@
 ### need to be changed for oring paas payload protocal
+'''
+components in a basic publisher script:
+    1. connector: 
+    2. request-constructor
+    3. a yaml config the connect params 
+'''
 import argparse
 import json
+import yaml
 
 import request_modules as rm
+import connector as cn
 
 module_dict = {'containerStatusCheck': rm.container_checker}
+connector_dict = {'mqtt': cn.}
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-m", "--module", help="the module you want the device to run", type=str, required=True)
 parser.add_argument(
     "-i", "--image", help="the target container's image, including the image tag", type=str)
+# parser.add_argument(
+#     "--host", help="mqtt broker ip", type=str, default="localhost")
 parser.add_argument(
-    "--host", help="mqtt broker ip", type=str, default="localhost")
+    "--protocol", help="the communication type", type=str, required=True)
 
 args = parser.parse_args()
 
@@ -20,8 +31,6 @@ if(args.image):
     args.image = args.image.strip(' ')
     if len(args.image.split(':')) == 1:
         raise NameError("Requires both the image and the tag!")
-
-args.module = args.module.strip(' ')
 
 
 requester = module_dict[args.module](image=args.image, host=args.host)
